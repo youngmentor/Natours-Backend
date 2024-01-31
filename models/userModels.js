5c40aa5fd41133b03a9fb35e237d12fc5d8715eb
youngmentor <olaleresuliton43@gmail.com>
2024-01-31 17:39:53 +0100

    added the createdAt object to the model

--- models/userModels.js
+++ models/userModels.js
@@ -1,28 +1,34 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    userName: { 
-        type: String, 
-        required: true,
-        trim: true
-    },
-    password: { 
-        type: String, 
-        required: true 
-    },
-    phoneNumber: {
-       type:  Number,
-       required: true
-    },
-    email: {
-       type: String,
-       required: true
-    },
-    address: {
-        type: String,
-        trim: true
-    },
-})
+  userName: {
+    type: String,
+    required: [true, 'user must provide a username'],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: Number,
+    required: [true, 'user must provide a number'],
+  },
+  email: {
+    type: String,
+    required: [true, 'user must provide an email'],
+    unique: true,
+  },
+  address: {
+    type: String,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
+});
 
 const User = mongoose.model('User', userSchema);
 
